31d7a999332eceff6584454fff004e20cda7288f
Yana Suvorova <suvorova_ya@outlook.com>
2022-07-25 15:32:47 +0300

    added djangoD2

---
+++ djangoD2/protect/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
+
+
+
